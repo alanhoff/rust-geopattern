de339a5f1fe9156b6b30f0221e79d0901c4d3578
Alan Hoffmeister <alanhoffmeister@gmail.com>
2018-09-15 20:32:53 -0300

    Enable rendering to PNG

--- readme.md
+++ readme.md
@@ -4,5 +4,7 @@ Geopattern Server
 Run it with `cargo run --release` then access images using the following URL:
 
 ```
-http://localhost:3000/generate/:anything
+http://localhost:3000/svg/:anything
+http://localhost:3000/png/:anything
+http://localhost:3000/png/:anything/:size
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,152 @@
 extern crate geopattern;
 extern crate iron;
+extern crate resvg;
 extern crate router;
 
+use geopattern::patterns::Patterns;
 use iron::headers::{CacheControl, CacheDirective, ContentType};
 use iron::mime::{Mime, SubLevel, TopLevel};
 use iron::prelude::*;
+use resvg::backend_cairo::render_to_image;
+use resvg::usvg;
+use resvg::SizeExt;
 use router::Router;
 
+#[derive(Debug)]
+enum Mode {
+    Unknown,
+    Png,
+    Svg,
+}
+
+#[derive(Debug)]
+struct Pattern {
+    pub hash: String,
+    pub size: u32,
+    pub mode: Mode,
+}
+
+impl Pattern {
+    pub fn from_request(req: &Request) -> Self {
+        let hash = req
+            .extensions
+            .get::<Router>()
+            .unwrap()
+            .find("hash")
+            .unwrap_or("default")
+            .into();
+
+        let size = req
+            .extensions
+            .get::<Router>()
+            .unwrap()
+            .find("size")
+            .unwrap_or("128")
+            .parse::<u32>()
+            .unwrap_or(128);
+
+        let mode = match req
+            .extensions
+            .get::<Router>()
+            .unwrap()
+            .find("mode")
+            .unwrap_or("unknown")
+            .into()
+        {
+            "png" => Mode::Png,
+            "svg" => Mode::Svg,
+            _ => Mode::Unknown,
+        };
+
+        Pattern { hash, size, mode }
+    }
+
+    pub fn to_svg(&self) -> String {
+        geopattern::GeoPattern::new(&self.hash)
+            .patterns(&[
+                Patterns::Plaid,
+                Patterns::ConcentricCircles,
+                Patterns::MosaicSquares,
+                Patterns::Xes,
+                Patterns::Octagons,
+                Patterns::OverlappingCircles,
+                Patterns::PlusSigns,
+                Patterns::Squares,
+            ])
+            .build()
+            .unwrap()
+            .to_minified_svg()
+            .unwrap()
+    }
+
+    pub fn rasterize(&self) -> Vec<u8> {
+        let svg = self.to_svg();
+        let mut buffer = vec![];
+
+        let mut opts = resvg::Options::default();
+        opts.usvg.dpi = 300.0;
+
+        let rtree = usvg::Tree::from_str(&svg, &opts.usvg);
+        let screensize = rtree.svg_node().size.to_screen_size();
+
+        let zoom = self.size as f32 / std::cmp::min(screensize.width, screensize.height) as f32;
+        opts.fit_to = resvg::FitTo::Zoom(zoom);
+
+        render_to_image(&rtree, &opts)
+            .unwrap()
+            .write_to_png(&mut buffer)
+            .unwrap();
+
+        buffer
+    }
+
+    pub fn build_response(&self) -> IronResult<Response> {
+        match self.mode {
+            Mode::Unknown => Ok(Response::with((iron::status::NotFound, "Not found"))),
+            Mode::Svg => {
+                let svg = self.to_svg();
+                let mut res = Response::with((iron::status::Ok, svg));
+
+                res.headers.set(ContentType(Mime(
+                    TopLevel::Image,
+                    SubLevel::Ext("svg+xml".to_string()),
+                    vec![],
+                )));
+
+                res.headers.set(CacheControl(vec![
+                    CacheDirective::Public,
+                    CacheDirective::MaxAge(2592000),
+                ]));
+
+                Ok(res)
+            }
+            Mode::Png => {
+                let buffer = self.rasterize();
+                let mut res = Response::with((iron::status::Ok, buffer));
+
+                res.headers.set(ContentType::png());
+
+                res.headers.set(CacheControl(vec![
+                    CacheDirective::Public,
+                    CacheDirective::MaxAge(2592000),
+                ]));
+
+                Ok(res)
+            }
+        }
+    }
+}
+
 fn generate(req: &mut Request) -> IronResult<Response> {
-    let hash = req
-        .extensions
-        .get::<Router>()
-        .unwrap()
-        .find("hash")
-        .unwrap();
-    let pattern = geopattern::generate(hash);
-    let mut res = Response::with((iron::status::Ok, pattern.to_svg().unwrap().to_string()));
-
-    res.headers.set(ContentType(Mime(
-        TopLevel::Image,
-        SubLevel::Ext("svg+xml".to_string()),
-        vec![],
-    )));
-
-    res.headers.set(CacheControl(vec![
-        CacheDirective::Public,
-        CacheDirective::MaxAge(2592000),
-    ]));
-
-    Ok(res)
+    Pattern::from_request(&req).build_response()
 }
 
 fn main() {
+    resvg::init();
+
     let mut router = Router::new();
-    router.get("/generate/:hash", generate, "generate");
+    router.get("/:mode/:hash", generate, "generate");
+    router.get("/:mode/:hash/:size", generate, "generate_size");
 
     Iron::new(router).http("0.0.0.0:3000").unwrap();
 }
